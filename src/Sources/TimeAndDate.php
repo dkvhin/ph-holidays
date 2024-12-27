445d780453c808b3720d767a0e2d969c6a7bbb6e
Ervin Musngi <ervinm@d7softwaresolutions.com>
2024-12-27 11:54:21 +0800

    fix: enconding

--- src/Sources/TimeAndDate.php
+++ src/Sources/TimeAndDate.php
@@ -35,7 +35,7 @@ class TimeAndDate
                     'User-Agent'                    => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
                     'upgrade-insecure-requests'     => 1,
                     'Accept'                        => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                    'Accept-Encoding'               => 'gzip, deflate, br, zstd',
+                    'Accept-Encoding'               => 'gzip, deflate, zstd',
                     'Accept-Language'               => 'en-US,en;q=0.9,vi;q=0.8,es;q=0.7,ar;q=0.6',
                     'Cache-Control'                 => 'no-cache'
                 ]
@@ -57,7 +57,7 @@ class TimeAndDate
 
             // Regular Holiday
             $table = $dom->getElementById('holidays-table');
-
+    
             if ($table) {
                 $tbody = $table->getElementsByTagName('tbody');
                 if ($item = $tbody->item(0)) {
